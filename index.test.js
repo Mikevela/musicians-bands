5ab2fff50d3fdb91566e33833773853006516f3c
zoemx <zoemichaux@google.com>
2023-10-10 16:03:32 -0500

    added song tests

--- index.test.js
+++ index.test.js
@@ -21,14 +21,23 @@ describe('Band, Musician, and Song Models', () => {
         expect(punkBand.name).toBe("linkin park");
     })
 
-    test('can create a Musician', async () => {
+    test('can create a song', async () => {
         const adele = await Musician.create({
             name: "hello",
             instrument: "voice"
         })
         // TODO - test creating a musician
         expect(adele.name).toBe("hello");
+
+        const song1 = await Song.create({
+            title : "OMG",
+            year: 2016,
+            length: 3
+        })
+        // TODO - test creating a band
+        expect(song1.title).toBe("OMG");
     })
+    //})
 
     test('can update a Band', async () => {
         const punkBand = await Band.create({
@@ -60,6 +69,23 @@ describe('Band, Musician, and Song Models', () => {
         expect(adeleUpdate.name).toBe("Beyonce");
     })
 
+    test('can update a song', async () => {
+        const song2 = await Song.create({
+        title: "hello",
+            year:2016,
+            length : 3
+        })
+
+       songUpdate = await song2.update({
+            title: "Boss"
+            
+        })
+
+        // TODO - test updating a musician
+        expect(songUpdate.title).toBe("Boss");
+    })
+    
+
     test('can delete a Band', async () => {
         punkBand = await Band.create({
             name: "FooFighters",
@@ -67,6 +93,16 @@ describe('Band, Musician, and Song Models', () => {
         })
         // TODO - test deleting a band
         expect(await punkBand.destroy()).toBe(punkBand);
+    })
+
+    test('can delete a song', async () => {
+        song3 = await Song.create({
+            title: "hello",
+            year: 1996,
+            length: 4
+        })
+        // TODO - test deleting a musician
+        expect(await song3.destroy()).toBe(song3);
     })
 
     test('can delete a Musician', async () => {
@@ -77,4 +113,5 @@ describe('Band, Musician, and Song Models', () => {
         // TODO - test deleting a musician
         expect(await adele.destroy()).toBe(adele);
     })
+
 })
